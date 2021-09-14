4989bc6c02e4a855ef7bd13f13fd4129a8d9472b
IAmNotAProgramer <dawidsce@gmail.com>
2021-09-14 23:34:10 +0200

    rtl: resolve problem with asynch reset

--- src/rtl/internal_reset.v
+++ src/rtl/internal_reset.v
@@ -16,6 +16,7 @@
 // Revision: 
 // Revision 0.01 - File Created
 // Revision 0.02 - Removes Counter
+// Revision 0.02 - Create flag
 // Additional Comments:         using Verilog-2001 syntax.
 //
 // The `timescale directive specifies what the
@@ -34,11 +35,33 @@ module internal_reset (
                 output  reg  reset_out
         );
 
+        reg [3:0] counter,counter_nxt;
+        reg flag,flag_nxt;
+        reg reset_nxt;
+
         always @(negedge clk or negedge locked) begin
                 if(!locked) begin
-                        reset_out <= 1'b1;
+                        counter <= 7'b0;
+                        flag    <= 1'b1;
                 end else begin 
-                        reset_out <= 1'b0;
+                        counter <= counter_nxt;
+                        flag    <= flag_nxt;
+                end
+                reset_out <= reset_nxt;
+        end
+
+        always @* begin
+                reset_nxt = reset_out;
+                if (flag == 1'b1)
+                        counter_nxt = counter + 1;
+                else
+                        counter_nxt = counter;
+                if (counter == 10) begin   
+                        reset_nxt = 1'b1;
+                        flag_nxt  = 1'b0;
+                end else begin
+                        reset_nxt = 1'b0;
+                        flag_nxt  = flag;
                 end
         end
 endmodule
